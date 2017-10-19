cb15fd68b3928a6880ffbfa93a350e9f5cd458b7
Hameed Gifford <giff.h92@gmail.com>
2017-10-19 15:37:23 -0500

    Started putting some snippets in the slides

---
+++ getting_started.py
@@ -0,0 +1,13 @@
+from reportlab.pdfgen.canvas import Canvas
+
+# This is your PDF writer
+pdf = Canvas("./getting_started.pdf")
+
+# Everything is coordinate based
+pdf.drawString(100, 100, "Hello, World!")
+
+# Writes everything to a page, then moves onto another one
+pdf.showPage()
+
+# Performs the write to disk
+pdf.save()
---
+++ helpful_lib_elements.py
@@ -0,0 +1,4 @@
+from reportlab.pdfgen.canvas import Canvas
+from reportlab.lib.colors import red, green, blue, chartreuse, plum, darkorchid
+from reportlab.lib.pagesizes import letter, legal, elevenSeventeen
+from reportlab.lib.styles import getSampleStyleSheet
