de7424426a9af67ffe3863528fc86465794e81f0
Hameed Gifford <giff.h92@gmail.com>
2017-10-19 15:59:55 -0500

    Put more into lib elements

--- helpful_lib_elements.py
+++ helpful_lib_elements.py
@@ -1,4 +1,15 @@
-from reportlab.pdfgen.canvas import Canvas
-from reportlab.lib.colors import red, green, blue, chartreuse, plum, darkorchid
+from reportlab.lib.colors import red, green, blue, chartreuse, plum, darkorchid, Color, HexColor, CMYKColor
 from reportlab.lib.pagesizes import letter, legal, elevenSeventeen
-from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle, ListStyle
+from reportlab.lib.units import inch, cm, mm
+
+
+# ParagraphStyle
+normal = getSampleStyleSheet()["Normal"]
+bullet = getSampleStyleSheet()["Bullet"]
+
+# ListStyle
+ul = getSampleStyleSheet()["UnorderedList"]
+
+# dict
+print(getSampleStyleSheet().byName)
