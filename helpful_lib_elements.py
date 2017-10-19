d4a20c5ac72eb3d2844c37d64e5ee26fc70fd1ca
Hameed Gifford <giff.h92@gmail.com>
2017-10-19 16:04:15 -0500

    Split imports for export readability

--- helpful_lib_elements.py
+++ helpful_lib_elements.py
@@ -1,6 +1,9 @@
-from reportlab.lib.colors import red, green, blue, chartreuse, plum, darkorchid, Color, HexColor, CMYKColor
-from reportlab.lib.pagesizes import letter, legal, elevenSeventeen
-from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle, ListStyle
+from reportlab.lib.colors import red, green, blue, \
+    chartreuse, plum, darkorchid, Color, HexColor, CMYKColor
+from reportlab.lib.pagesizes import letter, legal, \
+    elevenSeventeen
+from reportlab.lib.styles import getSampleStyleSheet, \
+    ParagraphStyle, ListStyle
 from reportlab.lib.units import inch, cm, mm
 
 
